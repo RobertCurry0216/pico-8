60871b95cf373f6be886891c21158b6eb16a09e2
robertcurry0216 <robertcurry0216@gmail.com>
2022-04-10 11:29:43 +1000

    some art stuff

--- games/picorausers/rooms/welcome.lua
+++ games/picorausers/rooms/welcome.lua
@@ -12,5 +12,21 @@ end
 
 function welcome:draw()
   cls(15)
-  cprint("picorausers", 64, 64, 2)
+
+  --skull
+  sspr(0,80, 24,25, 36,-1, 48,50)
+  sspr(10,106, 12,6, 50,56, 24,12)
+
+  
+  --heading
+  spr(128, 12, 46, 13, 2)
+
+  --water
+  poke(0x5f54, 0x60)
+  pal({1,14,3,4,5,6,6,8,9,10,11,12,13,14,7,0})
+  sspr(0,0, 128,64, 0,64, 128,64, false, true)
+  --reset GFX
+  poke(0x5f54, 0x00)
+  pal()
+
 end
